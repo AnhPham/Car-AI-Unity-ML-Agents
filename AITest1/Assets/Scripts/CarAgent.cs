3c04602c56cd1382e04b4b742be28f74369cec96
anh-pham <anhpt.csit@gmail.com>
2025-01-18 18:24:29 +0700

    Car

---
+++ AITest1/Assets/Scripts/CarAgent.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class CarAgent : Agent
+{
+    public Transform target;
+    public CarControl carControl;
+
+    Rigidbody rBody;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // If the Agent fell, zero its momentum
+        if (this.transform.localPosition.y < 0)
+        {
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0, 0);
+        }
+
+        // Move the target to a new spot
+        target.localPosition = new Vector3(Random.value * 8 - 4, 0, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        carControl.UpdateCar(controlSignal.x, controlSignal.z);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            var reward = Mathf.Clamp01(rBody.velocity.magnitude > 0 ? 0.5f / rBody.velocity.magnitude : 1);
+            //Debug.Log("Reward: " + reward);
+            SetReward(reward);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        else if (this.transform.localPosition.y <= -0.01f)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+        continuousActionsOut[1] = Input.GetAxis("Horizontal");
+    }
+}
